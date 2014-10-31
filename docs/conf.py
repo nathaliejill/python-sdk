7dc68a71963130dfaba30a116c6c3462d4f42cb7
Federico Repond <frepond@gmail.com>
2014-10-31 04:18:11 -0300

    Corrected copyright.

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Xapo SDK & Tools'
-copyright = u'2014, Federico Repond'
+copyright = u'2014, Xapo'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
